a5f24d4e5af39df156b7a889a2ff1a7341f79246
Cr4N31 <168254523+Cr4N31@users.noreply.github.com>
2025-07-25 13:44:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ AOS.init({
   easing: "ease-out",  
   once: false,  
 });
+
+const toggleBtn = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+    const backdrop = document.getElementById('backdrop');
+
+    toggleBtn.addEventListener('click', () => {
+      const isOpen = navMenu.classList.contains('translate-y-0');
+
+      if (isOpen) {
+        navMenu.classList.remove('translate-y-0');
+        navMenu.classList.add('translate-y-[-100%]');
+        backdrop.classList.add('hidden');
+      } else {
+        navMenu.classList.remove('translate-y-[-100%]');
+        navMenu.classList.add('translate-y-0');
+        backdrop.classList.remove('hidden');
+      }
+    });
+
+    backdrop.addEventListener('click', () => {
+      navMenu.classList.remove('translate-y-0');
+      navMenu.classList.add('translate-y-[-100%]');
+      backdrop.classList.add('hidden');
+    });
